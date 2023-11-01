f9ee744fe0aef4ba003233149c5328af1622eb34
Htar Htar Ei <htarhtarei670@example.com>
2023-11-01 16:32:36 +0630

    fix bug

--- src/Components/Home/Zodiac.jsx
+++ src/Components/Home/Zodiac.jsx
@@ -1,17 +1,30 @@
-import { useContext } from "react"
+import { useState } from "react"
+import { useContext ,useEffect} from "react"
 import { ZodiacContext } from "../../store/ZodiacContext"
 import SignDetail from "../Details/SignDetail"
 import Hero from "./Hero"
 
 const Zodiac = () => {
     const ctx=useContext(ZodiacContext);
-    window.scroll(top);
+    const [btnShow,setBtnShow]=useState(false);
         
+    window.scroll(top);
+
+    useEffect(() => {
+        // 👇️ scroll to top on page load
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+
+          }, []);
+
     return (
-        <div className="bg-orange-50">
+        <>
             {ctx.isShowDetail ? <SignDetail data={ctx.detail}/> : <Hero/>}
-            {/* <ScrollToTop smooth /> */}
-\        </div>
+            
+            {/* scrolll up btn */}
+            <button  onClick={() => {window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}} className="bg-orange-500 text-white fixed bottom-4 right-7 p-2 rounded-md">
+                <i className="ri-arrow-up-double-line text-xl"></i>
+            </button>
+        </>
     )
 }
 
---
+++ src/Components/ScrollUpBtn.jsx
@@ -0,0 +1,9 @@
+const ScrollUpBtn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ScrollUpBtn
